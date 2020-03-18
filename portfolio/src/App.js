d745d1e4d8690252efffb341176dfac22953cdac
saiduddukuri411 <61610604+saiduddukuri411@users.noreply.github.com>
2020-03-17 21:49:25 -0400

    created layout for portfolio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-
+import Layout from './components/layout/layout'
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+           <Layout>
+             <p>this is projects container</p>
+            </Layout>
       </div>
     );
   }
